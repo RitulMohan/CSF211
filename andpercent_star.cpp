0ba1fbffee1ce7e0993545e9d32e15e6c33dd465
RitulMohan <79750424+RitulMohan@users.noreply.github.com>
2022-01-17 12:27:36 +0530

    Add files via upload

---
+++ andpercent_star.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{ 
+   int x=10; 
+   cout<< &x<<endl;  
+   cout<<*(&x) ; 
+      return 0;   
+        
+}
+
+
+//star before the address will give us the value at the address 
